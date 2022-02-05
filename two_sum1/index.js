7adcbd3ddd6339db24f1f3bda31e56a86d2854e1
monu kumar <92091460+sahmk98@users.noreply.github.com>
2022-02-06 00:54:47 +0530

    Add files via upload

---
+++ two_sum1/index.js
@@ -0,0 +1,10 @@
+function twoSum(nums, target) {
+    let x, y;
+    x = nums.findIndex((num1, index1) => {
+        y = nums.findIndex((num2, index2) => (num1 + num2 === target) && (index1 !== index2));
+        return ~y;
+    })
+    return [x, y];
+};
+console.log(twoSum([2,7,11,15],9));
+document.write(twoSum([2,7,11,15],9));
