8454f33019817d45b244a27e204dca81eaa2f33c
monu kumar <92091460+sahmk98@users.noreply.github.com>
2022-02-06 19:59:36 +0530

    Add files via upload

---
+++ vowel/index.js
@@ -0,0 +1,15 @@
+var char = prompt("Enter the character");
+
+if(char == "a") {
+ alert("It is a vowel");
+} else if(char == "e") {
+ alert("It is a vowel");
+} else if(char == "i") {
+ alert("It is a vowel");
+} else if(char == "o") {
+ alert("It is a vowel");
+} else if(char == "u") {
+ alert("It is a vowel");
+} else {
+ alert("It is a consonant");
+}
